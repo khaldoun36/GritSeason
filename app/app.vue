cb9328bdf02695cd5be7f1fb2272938ceb71d0e7
khaldoun36 <khaldoun36@gmail.com>
2025-10-04 21:11:25 +0400

    first commit

---
+++ app/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="px-4">
+    <VitePwaManifest />
+    <div
+      v-for="n in 100"
+      :key="n"
+      class="w-full h-10 rounded-lg outline outline-black/10 mt-10"
+    />
+  </div>
+</template>
